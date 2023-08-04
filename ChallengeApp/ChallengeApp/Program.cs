f43a71f5bfe1fede402a5b3a1083f8458bf3c17e
Maciej <ghostek112@gmail.com>
2023-08-04 10:07:55 +0200

    Zadanie domowe - dzien 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,1 +1,24 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+
+Console.WriteLine("Podaj imię:");
+string imie=Console.ReadLine();
+Console.WriteLine("Podaj płeć M lub K:");
+char plec = char.Parse(Console.ReadLine());
+Console.WriteLine("Podaj wiek:");
+int wiek =int.Parse(Console.ReadLine());
+if (wiek == 33 && imie == "Ewa")
+{
+    Console.WriteLine("Masz na imię"+ imie +" i masz "+ wiek + " lat.");
+}
+else if (wiek < 18 && plec == 'M')
+{
+    Console.WriteLine("Jestes niepełnoletnim mężczyzną");
+}
+else if (wiek >= 18 && plec == 'M')
+{
+    Console.WriteLine("Jestes pełnoletnim mężczyzną");
+}
+else if (plec == 'K')
+{
+    Console.WriteLine("Jestes kobieta");
+}
