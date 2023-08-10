f78a638818ada546c02c38171c0a5e8b573edc52
Maciej <ghostek112@gmail.com>
2023-08-10 11:55:30 +0200

    Praca domowa 6 dzień

---
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -0,0 +1,21 @@
+﻿namespace ChallengeApp
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+
+        public List<int> Points = new List<int>();
+
+        public void AddPoint(int point)
+        {
+            Points.Add(point);
+        }
+
+        public int Sum()
+        {
+            return Points.Sum();
+        }
+    }
+}
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,24 +1,43 @@
-﻿
+﻿using ChallengeApp;
 
-Console.WriteLine("Podaj imię:");
-string imie=Console.ReadLine();
-Console.WriteLine("Podaj płeć M lub K:");
-char plec = char.Parse(Console.ReadLine());
-Console.WriteLine("Podaj wiek:");
-int wiek =int.Parse(Console.ReadLine());
-if (wiek == 33 && imie == "Ewa")
+Employee Marie = new Employee();
+Employee Nick = new Employee();
+Employee John = new Employee();
+Marie.Name = "Marie";
+Nick.Name = "Nick";
+John.Name = "John";
+
+Marie.AddPoint(1);
+Marie.AddPoint(2);
+Marie.AddPoint(3);
+Marie.AddPoint(4);
+Marie.AddPoint(5000);
+
+Nick.AddPoint(4);
+Nick.AddPoint(5);
+Nick.AddPoint(6);
+Nick.AddPoint(7);
+Nick.AddPoint(8);
+
+John.AddPoint(9);
+John.AddPoint(10);
+John.AddPoint(11);
+John.AddPoint(12);
+John.AddPoint(13);
+
+List<Employee> list = new List<Employee>();
+list.Add(Marie);
+list.Add(Nick);
+list.Add(John);
+int sum = 0;
+Employee BestEmployee = new Employee();
+foreach (Employee emp in list)
 {
-    Console.WriteLine("Masz na imię"+ imie +" i masz "+ wiek + " lat.");
+    if (emp.Sum() > sum)
+    {
+        sum = emp.Sum();
+        BestEmployee = emp;
+    }
 }
-else if (wiek < 18 && plec == 'M')
-{
-    Console.WriteLine("Jestes niepełnoletnim mężczyzną");
-}
-else if (wiek >= 18 && plec == 'M')
-{
-    Console.WriteLine("Jestes pełnoletnim mężczyzną");
-}
-else if (plec == 'K')
-{
-    Console.WriteLine("Jestes kobieta");
-}
+
+Console.WriteLine($"Pracownik z najwyższym wynikiem to {BestEmployee.Name} z wynikiem:{sum} punktów");
